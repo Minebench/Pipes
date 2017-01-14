c9b387ede7b1513d79b2001f3c2bde54c6d074d0
Max Lee <mail@moep.tv>
2017-01-14 18:56:11 +0100

    Also stop the output dropper to move it's filter items into the chest

--- src/main/java/io/github/apfelcreme/Pipes/Listener/BlockListener.java
+++ src/main/java/io/github/apfelcreme/Pipes/Listener/BlockListener.java
@@ -11,6 +11,7 @@ import io.github.apfelcreme.Pipes.PipesConfig;
 import io.github.apfelcreme.Pipes.PipesItem;
 import io.github.apfelcreme.Pipes.PipesUtil;
 import org.bukkit.Material;
+import org.bukkit.block.BlockState;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +20,7 @@ import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.ItemStack;
@@ -57,6 +59,14 @@ public class BlockListener implements Listener {
     @EventHandler
     private void onItemDispense(BlockDispenseEvent event) {
         if (PipesUtil.getPipesItem(event.getBlock()) != null) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryMove(InventoryMoveItemEvent event) {
+        if (event.getSource().getHolder() instanceof BlockState
+                && PipesItem.PIPE_OUTPUT.check(((BlockState) event.getSource().getHolder()).getBlock())) {
             event.setCancelled(true);
         }
     }
